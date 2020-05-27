961fa42e8a9d90dbbc91a7b97906d8d1b93e9e54
jimmytraoree <66011596+jimmytraoree@users.noreply.github.com>
2020-05-27 13:17:12 +0200

    Update full_ssd.py

--- full_ssd.py
+++ full_ssd.py
@@ -1,48 +1,3 @@
-
-Skip to content
-Pull requests
-Issues
-Marketplace
-Explore
-@jimmytraoree
-Learn Git and GitHub without any code!
-
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-pierluigiferrari /
-ssd_keras
-
-55
-1.6k
-
-    780
-
-Code
-Issues 7
-Pull requests 0
-Actions
-Projects 0
-Wiki
-Security 0
-
-    Insights
-
-ssd_keras/models/keras_ssd300.py /
-@pierluigiferrari pierluigiferrari chore: Switch to Apache license bd30eaa on May 2, 2018
-457 lines (398 sloc) 33.9 KB
-'''
-A Keras port of the original Caffe SSD300 network.
-Copyright (C) 2018 Pierluigi Ferrari
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-   http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
-
 from __future__ import division
 import numpy as np
 from keras.models import Model
@@ -84,112 +39,6 @@ def ssd_300(image_size,
             top_k=200,
             nms_max_output_size=400,
             return_predictor_sizes=False):
-    '''
-    Build a Keras model with SSD300 architecture, see references.
-    The base network is a reduced atrous VGG-16, extended by the SSD architecture,
-    as described in the paper.
-    Most of the arguments that this function takes are only needed for the anchor
-    box layers. In case you're training the network, the parameters passed here must
-    be the same as the ones used to set up `SSDBoxEncoder`. In case you're loading
-    trained weights, the parameters passed here must be the same as the ones used
-    to produce the trained weights.
-    Some of these arguments are explained in more detail in the documentation of the
-    `SSDBoxEncoder` class.
-    Note: Requires Keras v2.0 or later. Currently works only with the
-    TensorFlow backend (v1.0 or later).
-    Arguments:
-        image_size (tuple): The input image size in the format `(height, width, channels)`.
-        n_classes (int): The number of positive classes, e.g. 20 for Pascal VOC, 80 for MS COCO.
-        mode (str, optional): One of 'training', 'inference' and 'inference_fast'. In 'training' mode,
-            the model outputs the raw prediction tensor, while in 'inference' and 'inference_fast' modes,
-            the raw predictions are decoded into absolute coordinates and filtered via confidence thresholding,
-            non-maximum suppression, and top-k filtering. The difference between latter two modes is that
-            'inference' follows the exact procedure of the original Caffe implementation, while
-            'inference_fast' uses a faster prediction decoding procedure.
-        l2_regularization (float, optional): The L2-regularization rate. Applies to all convolutional layers.
-            Set to zero to deactivate L2-regularization.
-        min_scale (float, optional): The smallest scaling factor for the size of the anchor boxes as a fraction
-            of the shorter side of the input images.
-        max_scale (float, optional): The largest scaling factor for the size of the anchor boxes as a fraction
-            of the shorter side of the input images. All scaling factors between the smallest and the
-            largest will be linearly interpolated. Note that the second to last of the linearly interpolated
-            scaling factors will actually be the scaling factor for the last predictor layer, while the last
-            scaling factor is used for the second box for aspect ratio 1 in the last predictor layer
-            if `two_boxes_for_ar1` is `True`.
-        scales (list, optional): A list of floats containing scaling factors per convolutional predictor layer.
-            This list must be one element longer than the number of predictor layers. The first `k` elements are the
-            scaling factors for the `k` predictor layers, while the last element is used for the second box
-            for aspect ratio 1 in the last predictor layer if `two_boxes_for_ar1` is `True`. This additional
-            last scaling factor must be passed either way, even if it is not being used. If a list is passed,
-            this argument overrides `min_scale` and `max_scale`. All scaling factors must be greater than zero.
-        aspect_ratios_global (list, optional): The list of aspect ratios for which anchor boxes are to be
-            generated. This list is valid for all prediction layers.
-        aspect_ratios_per_layer (list, optional): A list containing one aspect ratio list for each prediction layer.
-            This allows you to set the aspect ratios for each predictor layer individually, which is the case for the
-            original SSD300 implementation. If a list is passed, it overrides `aspect_ratios_global`.
-        two_boxes_for_ar1 (bool, optional): Only relevant for aspect ratio lists that contain 1. Will be ignored otherwise.
-            If `True`, two anchor boxes will be generated for aspect ratio 1. The first will be generated
-            using the scaling factor for the respective layer, the second one will be generated using
-            geometric mean of said scaling factor and next bigger scaling factor.
-        steps (list, optional): `None` or a list with as many elements as there are predictor layers. The elements can be
-            either ints/floats or tuples of two ints/floats. These numbers represent for each predictor layer how many
-            pixels apart the anchor box center points should be vertically and horizontally along the spatial grid over
-            the image. If the list contains ints/floats, then that value will be used for both spatial dimensions.
-            If the list contains tuples of two ints/floats, then they represent `(step_height, step_width)`.
-            If no steps are provided, then they will be computed such that the anchor box center points will form an
-            equidistant grid within the image dimensions.
-        offsets (list, optional): `None` or a list with as many elements as there are predictor layers. The elements can be
-            either floats or tuples of two floats. These numbers represent for each predictor layer how many
-            pixels from the top and left boarders of the image the top-most and left-most anchor box center points should be
-            as a fraction of `steps`. The last bit is important: The offsets are not absolute pixel values, but fractions
-            of the step size specified in the `steps` argument. If the list contains floats, then that value will
-            be used for both spatial dimensions. If the list contains tuples of two floats, then they represent
-            `(vertical_offset, horizontal_offset)`. If no offsets are provided, then they will default to 0.5 of the step size.
-        clip_boxes (bool, optional): If `True`, clips the anchor box coordinates to stay within image boundaries.
-        variances (list, optional): A list of 4 floats >0. The anchor box offset for each coordinate will be divided by
-            its respective variance value.
-        coords (str, optional): The box coordinate format to be used internally by the model (i.e. this is not the input format
-            of the ground truth labels). Can be either 'centroids' for the format `(cx, cy, w, h)` (box center coordinates, width,
-            and height), 'minmax' for the format `(xmin, xmax, ymin, ymax)`, or 'corners' for the format `(xmin, ymin, xmax, ymax)`.
-        normalize_coords (bool, optional): Set to `True` if the model is supposed to use relative instead of absolute coordinates,
-            i.e. if the model predicts box coordinates within [0,1] instead of absolute coordinates.
-        subtract_mean (array-like, optional): `None` or an array-like object of integers or floating point values
-            of any shape that is broadcast-compatible with the image shape. The elements of this array will be
-            subtracted from the image pixel intensity values. For example, pass a list of three integers
-            to perform per-channel mean normalization for color images.
-        divide_by_stddev (array-like, optional): `None` or an array-like object of non-zero integers or
-            floating point values of any shape that is broadcast-compatible with the image shape. The image pixel
-            intensity values will be divided by the elements of this array. For example, pass a list
-            of three integers to perform per-channel standard deviation normalization for color images.
-        swap_channels (list, optional): Either `False` or a list of integers representing the desired order in which the input
-            image channels should be swapped.
-        confidence_thresh (float, optional): A float in [0,1), the minimum classification confidence in a specific
-            positive class in order to be considered for the non-maximum suppression stage for the respective class.
-            A lower value will result in a larger part of the selection process being done by the non-maximum suppression
-            stage, while a larger value will result in a larger part of the selection process happening in the confidence
-            thresholding stage.
-        iou_threshold (float, optional): A float in [0,1]. All boxes that have a Jaccard similarity of greater than `iou_threshold`
-            with a locally maximal box will be removed from the set of predictions for a given class, where 'maximal' refers
-            to the box's confidence score.
-        top_k (int, optional): The number of highest scoring predictions to be kept for each batch item after the
-            non-maximum suppression stage.
-        nms_max_output_size (int, optional): The maximal number of predictions that will be left over after the NMS stage.
-        return_predictor_sizes (bool, optional): If `True`, this function not only returns the model, but also
-            a list containing the spatial dimensions of the predictor layers. This isn't strictly necessary since
-            you can always get their sizes easily via the Keras API, but it's convenient and less error-prone
-            to get them this way. They are only relevant for training anyway (SSDBoxEncoder needs to know the
-            spatial dimensions of the predictor layers), for inference you don't need them.
-    Returns:
-        model: The Keras SSD300 model.
-        predictor_sizes (optional): A Numpy array containing the `(height, width)` portion
-            of the output tensor shape for each convolutional predictor layer. During
-            training, the generator function needs this in order to transform
-            the ground truth labels into tensors of identical structure as the
-            output tensors of the model, which is in turn needed for the cost
-            function.
-    References:
-        https://arxiv.org/abs/1512.02325v5
-    '''
 
     n_predictor_layers = 6 # The number of predictor conv layers in the network is 6 for the original SSD300.
     n_classes += 1 # Account for the background class.
@@ -291,6 +140,8 @@ def ssd_300(image_size,
     if swap_channels:
         x1 = Lambda(input_channel_swap, output_shape=(img_height, img_width, img_channels), name='input_channel_swap')(x1)
 
+        
+    # 
     conv1_1 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='conv1_1')(x1)
     conv1_2 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='conv1_2')(conv1_1)
     pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same', name='pool1')(conv1_2)
